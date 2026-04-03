dc4a69a27624152da3982bb003ed9e38a3583c41
Daria Eremeeva <yeremeeva-d@bk.ru>
2026-04-04 00:54:50 +0300

    добавила таблицы и миграции

---
+++ app/core/config.py
@@ -0,0 +1,22 @@
+from pydantic_settings import BaseSettings
+
+
+class Settings(BaseSettings):
+    POSTGRES_USER: str
+    POSTGRES_PASSWORD: str
+    POSTGRES_DB: str
+    POSTGRES_HOST: str
+    POSTGRES_PORT: int = 5432
+
+    SECRET_KEY: str
+    ALGORITHM: str = "HS256"
+
+    @property
+    def DATABASE_URL(self) -> str:
+        return f"postgresql+asyncpg://{self.POSTGRES_USER}:{self.POSTGRES_PASSWORD}@{self.POSTGRES_HOST}:{self.POSTGRES_PORT}/{self.POSTGRES_DB}"
+
+    class Config:
+        env_file = ".env"
+
+
+settings = Settings()
